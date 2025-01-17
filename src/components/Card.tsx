05abf2b7ec1fa3ca69bd27ed2fef64559e5b2c15
samuel <adeyemis710@gmail.com>
2025-01-18 00:18:55 +0100

    added button

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,23 @@
+import '../app/globals.css';
 import { layersIcon } from "@/assets";
 import Image from "next/image";
 
 export default function Card({ title, desc }: { title: string, desc: string }) {
     return (
         <div
-            className="w-full p-8 bg-blu-900 rounded-2xl flex flex-col gap-5 "
-            
+            className="w-full p-8 bg-blu-900 rounded-2xl flex flex-col gap-[2em] "
         >
             <div className="w-[46px] rounded-md flex items-center justify-center h-[46px] bg-[#60A6E799]">
                 <Image
                     src={layersIcon}
                     alt="stacks"
                     width={30}
-                    height={30} // Add height to prevent layout shift
+                    height={30}
                 />
             </div>
             <h2 className="text-2xl font-semibold">{title}</h2>
-            <h3 className="text-grey">{desc}</h3>
+            <h3 className="text-grey text-lg">{desc}</h3>
         </div>
+        
     );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,9 +2,9 @@ import Button from "./Button";
 
 export default function Hero(){
     return (
-        <section className="min-h-screen bg-[url('/images/header-bg.svg')] bg-no-repeat  px-3 bg-center">
+        <section className="min-h-screen bg-[url('/images/header-bg.svg')] bg-no-repeat  px-3 bg-center mt-[7em]">
         <div
-        className="rounded-[3em] max-w-5xl mx-auto bg-transparent bg-white bg-opacity-10  flex flex-col gap-5 px-5 md:py-[3em] py-[2em]"
+        className="rounded-[3em] max-w-6xl mx-auto bg-transparent bg-white bg-opacity-10  flex flex-col gap-5 px-5 md:py-[3em] py-[2em]"
         >
         <h1 className="text-[3.5rem] sm:text-[4rem] lg:text-[5rem] font-normal leading-[1em] mb-7">
             We build <span className="text-blu-100"> products </span> that shape a better future
